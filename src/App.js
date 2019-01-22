c50c7d4c31b827bfeced85153978c1351b013954
xhf394 <39177879+xhf394@users.noreply.github.com>
2019-01-23 07:22:02 +0800

    adjust infinite method

--- src/App.js
+++ src/App.js
@@ -5,16 +5,30 @@ import './App.css';
 const applyUpdateResult = (result) => (prevState) => ({
   hits: [...prevState.hits, ...result.hits],
   page: result.page,
+  
   //add loading status
   isLoading: false,
+
+  //track errors
+  isError: false,
 });
 
 const applySetResult = (result) => (prevState) => ({
   hits: result.hits,
   page: result.page,
+  
   //add loading status
   isLoading: false,
+
+  //track errors
+  isError: false,
 });
+
+//track error when it occurs
+const applySetError = (prevState) => ({
+  isError: true,
+  isLoading: false,
+})
 
 const getHackerNewsUrl = (value, page) =>
   `https://hn.algolia.com/api/v1/search?query=${value}&page=${page}&hitsPerPage=100`;
@@ -26,8 +40,12 @@ class App extends React.Component {
     this.state = {
       hits: [],
       page: null,
+      
       //track loading info
       isLoading: false,
+
+      //track error
+      isError: false,
     };
   }
 
@@ -51,11 +69,18 @@ class App extends React.Component {
   fetchStories = (value, page) => {
     //when request is made, loading property is set to true;
     this.setState({isLoading: true});
+    
     //when request is finished asynchronously, set back to false;
     fetch(getHackerNewsUrl(value, page))
       .then(response => response.json())
-      .then(result => this.onSetResult(result, page));
+      .then(result => this.onSetResult(result, page))
+      //set error handler
+      .catch(this.onSetError);
   }
+  
+  //error handler method
+  onSetError = () => 
+    this.setState(applySetError);
 
   onSetResult = (result, page) =>
     page === 0
@@ -74,12 +99,18 @@ class App extends React.Component {
         
         <List
           list={this.state.hits}
+          
           //add page state
           page={this.state.page}
+          
           //add onPaginated Search method to retrieve the next pages of paginated data
           onPaginatedSearch={this.onPaginatedSearch}
+          
           //loading status
           isLoading={this.state.isLoading}
+
+          //track errors
+          isError={this.state.isError}
         />
       </div>
     );
@@ -87,31 +118,68 @@ class App extends React.Component {
 }
 
 //pass all neccessary arguments to List component
-const List = ({ list, page, isLoading, onPaginatedSearch }) =>
-  <div>
-    <div className="list">
-      {list.map(item => <div className="list-row" key={item.objectID}>
-        <a href={item.url}>{item.title}</a>
-      </div>)}
-    </div>
+//update List component to ES6 class component
 
-    <div className="interactions"
-    //add loading status
-    >
-      {isLoading && <span>Loading...</span>}
-    </div>
-    <div className="interactions">
-      {
-        page !== null &&
-        <button
-          type="button"
-          onClick={onPaginatedSearch}
+class List extends React.Component {
+  componentDidMount() {
+    //add event listener to fetch list
+    window.addEventListener('scroll', this.onScroll, false);
+  }
+    
+  componentWillUnmount() {
+    //remove event listener
+    window.removeEventListener('scroll', this.onScroll, false);
+  }
+
+  onScroll = () => {
+    if(
+      //test when reach bottom
+      (window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 500) &&
+      
+      //already get back search value
+      this.props.list.length &&
+
+      //only trigger once reach bottom, no pending request, the scroll event will trigger 
+      !this.props.isLoading
+    ) {
+
+      //executes new pagniated search
+      this.props.onPaginatedSearch();
+    }
+  }
+
+  render() {
+    //pass arguments 
+    const {list, page, isLoading, onPaginatedSearch} = this.props;
+    
+    return (
+      <div>
+        <div className="list">
+          {list.map(item => <div className="list-row" key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </div>)}
+        </div>
+
+        <div className="interactions"
+        
+        //add loading status
         >
-        More
-        </button>
-      }
-    </div>
-
-  </div>
+          {isLoading && <span>Loading...</span>}
+        </div>
+        <div className="interactions">
+          {
+            page !== null &&
+            <button
+              type="button"
+              onClick={onPaginatedSearch}
+            >
+            More
+            </button>
+          }
+        </div>
+      </div>
+    )
+  }
+}
 
 export default App;
